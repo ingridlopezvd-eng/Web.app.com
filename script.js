e2f492c3bbc318685a4a989360975cf09c47c84f
ingridlopezvd-eng <ingridlopezvd@gmail.com>
2025-12-04 15:09:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ async function getAllRecords() {
 }
 
 getAllRecords();
+
+  let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let logo = data.records[i].fields["Logo"]; // here we are getting column values
+        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
+        let neighborhood = data.records[i].fields["Neighborhood"];
+
+        newHtml += `
+        
+         <div class="col-xl-4 cardImageText">
+          <div class="card list move border-dark mb-5" style="width: 20rem;">
+          <a href="breweries.html?id=${data.records[i].id}">${
+          logo
+            ? `<img class="card-img-top rounded" alt="${name}" src="${logo[0].url}">`
+            : ``
+        }
+          </a>
+          <p hidden class="card-key">${neighborhood}</p>
+          </div>
+          </div>
+        </div>
+    
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
+    });
+}
