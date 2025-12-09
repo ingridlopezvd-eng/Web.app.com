91f99fc93c40d12ad45de57a4794832620d12a22
ingridlopezvd-eng <ingridlopezvd@gmail.com>
2025-12-08 17:25:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getAllRecords() {
   const options = {
     method: "GET",
     headers: {
-      Authorization: `Bearer pateG7pBF1CkfmcW7.2c666498dc7818660958fea1c0bb95e5e1d33bbdb4871fed8ee5696394e05ce5`,
+      Authorization: `Bearer patkyHkDevXDLE79B.a2c80ae7b293e20d7376cd488319fe1b2d772d210f0ed063799e506eb0da7fa8`,
     },
   };
 
@@ -21,8 +21,8 @@ async function getAllRecords() {
     .then((data) => {
       console.log(data); // response is an object w/ .records array
 
-      getResultElement.innerHTML = "Community Resources"; // clear brews
-      let newHtml = "Community Resources";
+      getResultElement.innerHTML = "resources"; // clear brews
+      let newHtml = "resources";
 
       for (let i = 0; i < data.records.length; i++) {
         let Organization = data.records[i].fields["Organization"]; // here we are getting column values
@@ -30,7 +30,7 @@ async function getAllRecords() {
         let Employment = data.records[i].fields["Employment"];
         let Legalservices = data.records[i].fields["Legalservices"];
         let Otherresources = data.records[i].fields["Otherresources"];
-        let Contactc = data.records[i].fields["Contactc"];
+        let Contact = data.records[i].fields["Contact"];
         let Location = data.records[i].fields["Location"];
         let Link = data.records[i].fields["Link"];
         let Logo = data.records[i].fields["Logo"];
@@ -39,21 +39,24 @@ async function getAllRecords() {
         newHtml += `
         
         <div class="card" style="width: 18rem;">
-  <img src="${Organization[0].url}" class="card-img-top" alt= "${Organization}">
-  <div class="card-body">
-    <h5 class="card-title">${Organization}</h5>
+            <img src="${Organization[0].url}" class="card-img-top" alt= "${Organization}">
+            <div class="card-body">
+              <h5 class="card-title">${Organization}</h5>
              <p><strong>Housing:</strong> ${Housing || "N/A"}</p>
               <p><strong>Employment:</strong> ${Employment || "N/A"}</p>
               <p><strong>Legal Services:</strong> ${Legalservices || "N/A"}</p>
               <p><strong>Other Resources:</strong> ${Otherresources || "N/A"}</p>
               <p><strong>Contact:</strong> ${Contact || "N/A"}</p>
               <p><strong>Location:</strong> ${Location || "N/A"}</p>
+              <p><strong>Link:</strong> ${Link || "N/A"}</p>
+              <p><strong>Logo:</strong> ${Logo || "N/A"}</p>
+
 
               ${Link ? `<a href="${Link}" target="_blank" class="btn btn-primary">Visit Website</a>` : ""}
             </div>
           </div>
         `;
-      });
+      };
 
       getResultElement.innerHTML = newHtml;
     });
